e35d61854f9f80b51cd14cf3d83daa955ab4caa5
didwogh132 <62732120+didwogh132@users.noreply.github.com>
2025-08-31 18:39:23 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 212 ms, Memory: 8708 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<cstring>
+#include<cstring>
+
+using namespace std;
+
+int n, m, r;
+vector<int>arr[100010];
+int visited[100010];
+int cnt;
+
+void bfs(int start) {
+	queue<int>q;
+	q.push(start);
+	visited[start] = 1;
+	cnt = 1;
+
+	while (!q.empty()) {
+		int now = q.front(); q.pop();
+
+		for (int i = 0; i < arr[now].size(); i++) {
+			int next = arr[now][i];
+
+			if (visited[next] > 0) continue;
+			cnt++;
+			visited[next] = cnt;
+			q.push(next);
+		}
+	}
+}
+
+int main() {
+	
+	cin >> n >> m >> r;
+
+	for (int i = 0; i < m; i++) {
+		int from, to;
+		cin >> from >> to;
+		arr[from].push_back(to);
+		arr[to].push_back(from);
+	}
+	for (int i = 1; i <= n; i++) {
+		sort(arr[i].begin(), arr[i].end(), greater<>());
+	}
+
+	bfs(r);
+
+	for (int i = 1; i <= n; i++) {
+		cout << visited[i] << "\n";
+	}
+
+	return 0;
+}
