eb7b808401875f9e846bf2e7590e47d65e3286e3
didwogh132 <62732120+didwogh132@users.noreply.github.com>
2025-08-31 18:56:34 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 104 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct node {
+	int y;
+	int x;
+};
+vector<node> arr;
+
+bool cmp(node a, node b) {
+	if (a.y < b.y) return true;
+	if (a.y > b.y) return false;
+
+	if (a.x < b.x) return true;
+	if (a.x > b.x) return false;
+	return false;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int y, x;
+		cin >> y >> x;
+		arr.push_back({ y,x });
+	}
+	sort(arr.begin(), arr.end(), cmp);
+
+	for (int i = 0; i < arr.size(); i++) {
+		cout << arr[i].y << " " << arr[i].x << "\n";
+	}
+
+	return 0;
+}
