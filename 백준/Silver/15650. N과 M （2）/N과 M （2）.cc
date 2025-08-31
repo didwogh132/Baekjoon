cdfb4a1ade02d1c92b195993fe3e57f4e3e749d5
didwogh132 <62732120+didwogh132@users.noreply.github.com>
2025-08-31 18:47:27 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+int used[9];
+int path[9];
+
+void func(int now) {
+	if (now >= m) {
+		for (int i = 0; i < now; i++) {
+			cout << path[i] << " ";
+		}
+		cout << "\n";
+
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (used[i] == 1) continue;
+		if (now > 0 && path[now-1] > i) continue;
+		used[i] = 1;
+		path[now] = i;
+		func(now + 1);
+		used[i] = 0;
+		path[now] = 0;
+	}
+
+}
+
+int main() {
+	
+	cin >> n >> m;
+
+	func(0);
+	
+
+	return 0;
+}
