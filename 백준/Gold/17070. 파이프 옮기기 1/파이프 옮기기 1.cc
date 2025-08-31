96bf73c40f986268e5e2186baa9e24bab8a1e2ab
didwogh132 <62732120+didwogh132@users.noreply.github.com>
2025-08-31 18:21:18 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[20][20];
+int visited[20][20][3];
+
+int main() {
+    
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    visited[0][1][0] = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (arr[i][j] == 1) continue;
+            if (i - 1 >= 0) {
+                visited[i][j][1] += visited[i - 1][j][1] + visited[i - 1][j][2];
+            }
+
+            if (j - 1 >= 0) {
+                visited[i][j][0] += visited[i][j - 1][0] + visited[i][j - 1][2];
+            }
+
+            if (i - 1 >= 0 && j - 1 >= 0) {
+                if (arr[i - 1][j] == 0 && arr[i][j - 1] == 0) {
+                    visited[i][j][2] += visited[i - 1][j - 1][0] + visited[i - 1][j - 1][1] + visited[i - 1][j - 1][2];
+                }
+            }
+        }
+    }
+    
+    cout << visited[n - 1][n - 1][0] + visited[n - 1][n - 1][1] + visited[n - 1][n - 1][2];
+    return 0;
+}
