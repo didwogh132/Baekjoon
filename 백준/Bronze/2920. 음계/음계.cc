1bea86348a21992a1e97f21b0ab1b073ea7c6ed9
didwogh132 <62732120+didwogh132@users.noreply.github.com>
2025-09-13 20:31:48 +0900

    [Bronze II] Title: 음계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.cc
+++ 백준/Bronze/2920. 음계/음계.cc
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
-using namespace std;
-
-
 int main() {
     unsigned int pack = 0x00;
 
@@ -12,28 +9,9 @@ int main() {
         pack = (pack << 3) | (unsigned int)(x - 1);
     }
 
-    unsigned int asc = 0x00;
-    unsigned int desc = 0x00;
-
-    for (int i = 0; i < 8; i++) {
-        asc = (asc << 3) | (unsigned int)i;
-    }
-
-    for (int i = 7; i >= 0; i--) {
-        desc = (desc << 3) | (unsigned int)i;
-    }
-
-    if (pack == asc) {
-        printf("ascending");
-    }
-    else if (pack == desc) {
-        printf("descending");
-    }
-    else {
-        printf("mixed");
-    }
-
-
+    if (pack == 0x53977) printf("ascending");
+    else if (pack == 0xFAC688) printf("descending");
+    else printf("mixed");
 
     return 0;
 }
