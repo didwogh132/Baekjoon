3b485103aa0a3b843fc0cadbeecf2fd6d5bfae76
didwogh132 <62732120+didwogh132@users.noreply.github.com>
2025-09-13 20:13:17 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int h, m;
+    cin >> h >> m;
+
+    if (m < 45) {
+        h = (h + 23) % 24;
+        m = m + 15;
+    }
+    else {
+        m = m - 45;
+    }
+
+    cout << h << " " << m;
+    return 0;
+}
