9e5288d8696f76b13a653fa93f50c0bc39d753d1
didwogh132 <62732120+didwogh132@users.noreply.github.com>
2025-10-16 08:38:38 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -1,85 +1,68 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <queue>
-#include <climits>
-#include <memory.h>
+#include <stack>
 
 using namespace std;
 
 int n;
-int arr[1010];
-int visited[1010];
-int back_arr[1010];
-vector<int> num;
-struct node {
-    int num;
-    int idx;
-    int cost;
-    bool operator<(node right) const {
-        return cost < right.cost;
-    }
-};
-priority_queue<node> pq;
-
-void bfs() {
-
-    while (!pq.empty()) {
-        node now = pq.top(); pq.pop();
-
-        if (visited[now.idx] > now.cost) continue;
-
-        for (int i = now.idx + 1; i < n; i++) {
-            int nextnum = arr[i];
-            int nextidx = i;
-            int nextcost = now.cost + 1;
-            if (nextnum <= now.num) continue;
-            if (visited[nextidx] >= nextcost) continue;
-            visited[nextidx] = nextcost;
-            back_arr[nextidx] = now.idx;
-            pq.push({ nextnum, nextidx, nextcost });
-        }
-    }
-}
+int arr[1005];
+int pos[1005];
+int back_idx[1005];
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
 
-    cin >> n;
-    fill(visited, visited + n + 1, 0);
-    memset(back_arr, -1, sizeof(arr));
-    int a = INT_MAX;
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-        if (arr[i] <= a) {
-            a = arr[i];
-            visited[i] = 1;
-            pq.push({ arr[i], i, 1 });
-        }
-    }
+	cin >> n;
 
-    bfs();
+	for (int i = 0; i < n; i++) cin >> arr[i];
 
-    int max = 0;
-    int max_idx;
+	vector<int> lis;
+	vector<int> lis_idx;
 
-    for (int i = 0; i < n; i++) {
-        if (visited[i] > max) {
-            max = visited[i];
-            max_idx = i;
-        }
-    }
-    
-    for (int i = max_idx; i != -1; i = back_arr[i]) {
-        num.push_back(arr[i]);
-    }
+	for (int i = 0; i < n; i++) {
+		int now = arr[i];
+		int idx = lower_bound(lis.begin(), lis.end(), now) - lis.begin();
 
-    cout << max << "\n";
+		if (idx == lis.size()) {
+			lis.push_back(now);
+			lis_idx.push_back(i);
+		}
+		else {
+			lis[idx] = now;
+			lis_idx[idx] = i;
+		}
+		pos[i] = idx;
 
-    for (int i = num.size() - 1; i >= 0; i--) {
-        cout << num[i] << " ";
-    }
+		if (idx > 0) {
+			back_idx[i] = lis_idx[idx - 1];
+		}
+		else {
+			back_idx[i] = -1;
+		}
+	}
+	int length = lis.size();
+	cout << length << "\n";
 
-    return 0;
+	int last_idx = -1;
+	for (int i = n - 1; i >= 0; i--) {
+		if (pos[i] == length - 1) {
+			last_idx = i;
+			break;
+		}
+	}
+
+	stack<int> st;
+
+	while (last_idx != -1) {
+		st.push(arr[last_idx]);
+		last_idx = back_idx[last_idx];
+	}
+
+	while (!st.empty()) {
+		cout << st.top() << " ";
+		st.pop();
+	}
+	return 0;
 }
