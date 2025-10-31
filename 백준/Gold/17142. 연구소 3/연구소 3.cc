d1e14d7c96e37acf96d3dcd2405bdd480a40387a
didwogh132 <62732120+didwogh132@users.noreply.github.com>
2025-10-31 22:26:06 +0900

    [Gold III] Title: 연구소 3, Time: 12 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.cc
+++ 백준/Gold/17142. 연구소 3/연구소 3.cc
@@ -1,99 +1,87 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
-#include <algorithm>
 #include <queue>
-#include <climits>
+#include <memory.h>
 
 using namespace std;
 
 int n, m;
 int arr[52][52];
 int visited[52][52];
-int used[260];
 int ydir[] = { -1, 1, 0, 0 };
 int xdir[] = { 0, 0, -1, 1 };
-
 struct node {
     int y;
     int x;
 };
-
-vector<node> vir;
-vector<node> use_vir;
+vector<node> virus;
+vector<node> use_virus;
 queue<node> q;
+int ans = 3000;
 int Max;
-int Min = INT_MAX;
 
 void bfs() {
+    Max = 0;
     while (!q.empty()) {
         node now = q.front(); q.pop();
+
         for (int i = 0; i < 4; i++) {
             int ny = now.y + ydir[i];
             int nx = now.x + xdir[i];
             if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
             if (arr[ny][nx] == 1) continue;
-            if (visited[ny][nx] > visited[now.y][now.x] + 1) {
-                visited[ny][nx] = visited[now.y][now.x] + 1;
-            }
-            else if (visited[ny][nx] > -1) continue;
-            else {
-                visited[ny][nx] = visited[now.y][now.x] + 1;
-            }
-            q.push({ ny,nx });
+            if (visited[ny][nx] > -1) continue;
+            visited[ny][nx] = visited[now.y][now.x] + 1;
+            q.push({ ny, nx });
         }
     }
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (arr[i][j] == 0 && visited[i][j] > Max) Max = visited[i][j];
-            else if (arr[i][j] == 0 && visited[i][j] == -1) {
+            if (arr[i][j] == 0 && visited[i][j] == -1) {
                 Max = -1;
                 return;
             }
+            else if (arr[i][j] == 0 && visited[i][j] > Max) Max = visited[i][j];
         }
     }
 }
 
-void func(int num, int idx) {
+void dfs(int num, int idx) {
     if (num == m) {
         memset(visited, -1, sizeof(visited));
-        Max = 0;
-        for (int i = 0; i < use_vir.size(); i++) {
-            q.push(use_vir[i]);
-            visited[use_vir[i].y][use_vir[i].x] = 0;
+        for (int i = 0; i < use_virus.size(); i++) {
+            q.push(use_virus[i]);
+            visited[use_virus[i].y][use_virus[i].x] = 0;
         }
         bfs();
-        if (Max != -1 && Max < Min) Min = Max;
+
+        if (Max != -1 && Max < ans) ans = Max;
         return;
     }
-    for (int i = idx; i < vir.size(); i++) {
-        if (used[i] == 1) continue;
-        used[i] == 1;
-        use_vir.push_back(vir[i]);
-        func(num + 1, i + 1);
-        used[i] == 0;
-        use_vir.pop_back();
+    for (int i = idx; i < virus.size(); i++) {
+        use_virus.push_back(virus[i]);
+        dfs(num + 1, i + 1);
+        use_virus.pop_back();
     }
 }
 
 int main() {
-    
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     cin >> n >> m;
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> arr[i][j];
-            if (arr[i][j] == 2) {
-                vir.push_back({ i, j });
-            }
+            if (arr[i][j] == 2) virus.push_back({ i, j });
         }
     }
-    func(0, 0);
-    if (Min == INT_MAX) {
-        cout << -1;
-    }
-    else {
-        cout << Min;
-    }
+    dfs(0, 0);
+
+    if (ans == 3000) cout << -1;
+    else cout << ans;
 
     return 0;
 }
