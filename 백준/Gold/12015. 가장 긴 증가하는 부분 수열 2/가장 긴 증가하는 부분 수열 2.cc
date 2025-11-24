06922431796bd79ff877f3981f26bc8e377e1f59
didwogh132 <62732120+didwogh132@users.noreply.github.com>
2025-11-25 08:21:16 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 144 ms, Memory: 12200 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int arr[1000010];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	vector<int> lis;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) cin >> arr[i];
+
+	for (int i = 0; i < n; i++) {
+		if (lis.empty() || lis[lis.size() - 1] < arr[i]) {
+			lis.push_back(arr[i]);
+		}
+		else {
+			int pos = lower_bound(lis.begin(), lis.end(), arr[i]) - lis.begin();
+			lis[pos] = arr[i];
+		}
+	}
+
+	cout << lis.size();
+
+	return 0;
+}
