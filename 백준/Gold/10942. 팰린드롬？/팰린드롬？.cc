cc81ee57ed6e8eadc9dadd680da1c2192997e2e5
didwogh132 <62732120+didwogh132@users.noreply.github.com>
2025-11-30 21:45:43 +0900

    [Gold IV] Title: 팰린드롬?, Time: 200 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <memory.h>
+
+using namespace std;
+
+bool dp[2001][2001];
+int arr[2001];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m;
+
+    memset(dp, false, sizeof(dp));
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) cin >> arr[i];
+
+    for (int i = 1; i <= n; i++) dp[i][i] = true;
+
+    for (int len = 2; len <= n; len++) {
+        for (int s = 1; s + len - 1 <= n; s++) {
+            int e = s + len - 1;
+
+            if (len == 2) {
+                if (arr[s] == arr[e]) {
+                    dp[s][e] = true;
+                }
+                else {
+                    dp[s][e] = false;
+                }
+            }
+            else {
+                if (arr[s] == arr[e] && dp[s+1][e-1]) {
+                    dp[s][e] = true;
+                }
+                else {
+                    dp[s][e] = false;
+                }
+            }
+        }
+    }
+
+    cin >> m;
+
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        if (dp[a][b]) {
+            cout << 1 << "\n";
+        }
+        else {
+            cout << 0 << "\n";
+        }
+    }
+
+    return 0;
+}
