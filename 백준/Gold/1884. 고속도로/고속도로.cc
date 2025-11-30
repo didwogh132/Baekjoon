a1851952350c3940d94534a366a04202d424211f
didwogh132 <62732120+didwogh132@users.noreply.github.com>
2025-11-30 23:14:35 +0900

    [Gold I] Title: 고속도로, Time: 4 ms, Memory: 1764 KB -BaekjoonHub

--- 백준/Gold/1884. 고속도로/고속도로.cc
+++ 백준/Gold/1884. 고속도로/고속도로.cc
@@ -8,9 +8,7 @@ using namespace std;
 
 int k, n, r;
 struct node {
-    int num;
-    int line;
-    int cost;
+    int num, line, cost;
     bool operator <(const node& right) const {
         return line > right.line;
     }
@@ -23,25 +21,20 @@ void dijkstra(int num) {
     priority_queue<node> pq;
     pq.push({ num, 0, 0 });
     dist[0][num] = 0;
-
     while (!pq.empty()) {
         node now = pq.top(); pq.pop();
-
         if (dist[now.cost][now.num] < now.line) continue;
-        if(now.line >= ans ) continue;
-        
+        if(now.line >= ans) continue;
         if(now.num == n) {
             ans = (now.line < ans) ? now.line : ans;
+            continue;
         }
-
         for (int i = 0; i < arr[now.num].size(); i++) {
             node next = arr[now.num][i];
             int nextline = now.line + next.line;
             int nextcost = now.cost + next.cost;
 
-            if (nextcost > k) continue;
-            if(nextline >= ans) continue;
-            if (dist[nextcost][next.num] <= nextline) continue;
+            if(nextcost > k || nextline >= ans || dist[nextcost][next.num] <= nextline) continue;
             dist[nextcost][next.num] = nextline;
             pq.push({ next.num, nextline, nextcost });
         }
@@ -52,20 +45,13 @@ int main() {
     scanf("%d %d %d", &k, &n, &r);
 
     dist.assign(k + 1, vector<int>(n + 1, INT_MAX));
-
     for (int i = 0; i < r; i++) {
         int from, to, line, cost;
         scanf("%d %d %d %d", &from, &to, &line, &cost);
         arr[from].push_back({ to, line, cost });
     }
-
     dijkstra(1);
-
-    if (ans == INT_MAX) {
-        printf("-1");
-    }
-    else {
-        printf("%d", ans);
-    }
+    if (ans == INT_MAX) printf("-1");
+    else printf("%d", ans);
     return 0;
 }
