f3e1e2335297b7a137eb07ebab0579e2e3331a5a
didwogh132 <62732120+didwogh132@users.noreply.github.com>
2025-12-10 22:03:31 +0900

    [Gold IV] Title: Dart Challenge, Time: 20 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/7503. Dart Challenge/Dart Challenge.cc
+++ 백준/Gold/7503. Dart Challenge/Dart Challenge.cc
@@ -1,12 +1,9 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
-#include <memory.h>
+#include <bitset>
 
 using namespace std;
-
-bool single[301];
-bool dp[51][15001];
-int vals[301];
 
 int main() {
     ios::sync_with_stdio(false);
@@ -17,73 +14,31 @@ int main() {
 
     for (int tc = 1; tc <= t; tc++) {
         int n, dart;
-        int score[101];
-        int max_score = 0, second_score = 0;
 
-        memset(single, false, sizeof(single));
-        memset(dp, false, sizeof(dp));
+        bitset<15005> dp;
+        dp.set(0);
 
         cin >> n >> dart;
 
+        vector<int> score(n);
+
         for (int i = 0; i < n; i++) cin >> score[i];
 
-        sort(score, score + n);
+        sort(score.begin(), score.end());
 
-        single[0] = true;
-        for (int i = 0; i < n; i++) {
-            int v = score[i];
-            if (i == n - 1) {
-                single[v] = true;
-                single[v * 2] = true;
+        for (int i = 0; i < dart; i++) {
+            bitset<15005> ndp = dp;
+            for (int j = 0; j < n - 1; j++) {
+                ndp |= (dp << score[j]);
+                ndp |= (dp << score[j] * 2);
+                ndp |= (dp << score[j] * 3);
             }
-            else {
-                single[v] = true;
-                single[v * 2] = true;
-                single[v * 3] = true;
-            }
-        }
-        int max_single = 0;
-        int cnt_vals = 0;
-        for (int i = 0; i <= 300; i++) {
-            if (single[i]) {
-                vals[cnt_vals++] = i;
-                if (i > max_single) max_single = i;
-            }
+            ndp |= (dp << score.back());
+            ndp |= (dp << score.back() * 2);
+            dp = ndp;
         }
 
-        int max_total = max_single * dart;
-        int max_reach_prev = 0;
-        dp[0][0] = true;
-
-        for (int i = 1; i <= dart; i++) {
-            for (int s = 0; s <= max_total; s++) {
-                dp[i][s] = dp[i - 1][s];
-            }
-
-            int max_reach_cur = max_reach_prev;
-
-            for (int s = 0; s <= max_reach_prev; s++) {
-                if (!dp[i - 1][s]) continue;
-
-                for (int idx = 0; idx < cnt_vals; idx++) {
-                    int add = vals[idx];
-                    int ns = s + add;
-                    if (ns > max_total) continue;
-                    if (!dp[i][ns]) {
-                        dp[i][ns] = true;
-                        if (ns > max_reach_cur) max_reach_cur = ns;
-                    }
-                }
-            }
-            max_reach_prev = max_reach_cur;
-        }
-
-        int ans = 0;
-        for (int s = 0; s <= max_total; s++) {
-            if (dp[dart][s]) ans++;
-        }
-
-        cout << "Scenario #" << tc << ":" << "\n" << ans << "\n\n" ;
+        cout << "Scenario #" << tc << ":" << "\n" << dp.count() << "\n\n" ;
     }
 
     return 0;
