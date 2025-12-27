300804dffbb8f5644fcc6cc012ada2aa72ef542c
didwogh132 <62732120+didwogh132@users.noreply.github.com>
2025-12-27 12:17:51 +0900

    [Silver V] Title: K번째 수, Time: 952 ms, Memory: 21552 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> arr;
+    arr.resize(n);
+
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr.begin(), arr.end());
+
+    cout << arr[k - 1];
+    return 0;
+}
