148b77a5a82e47dcf0aaf65c67b48fa6e2394daf
didwogh132 <62732120+didwogh132@users.noreply.github.com>
2026-01-03 20:55:59 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 8 ms, Memory: 2924 KB -BaekjoonHub

---
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<int> arr;
+    vector<int> lis;
+    arr.resize(n);
+
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    for (int i = 0; i < n; i++) {
+        if (lis.empty() || lis[lis.size() - 1] < arr[i]) {
+            lis.push_back(arr[i]);
+        }
+        else {
+            int idx = lower_bound(lis.begin(), lis.end(), arr[i]) - lis.begin();
+            lis[idx] = arr[i];
+        }
+    }
+
+    cout << n - lis.size();
+    return 0;
+}
