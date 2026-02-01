624d083bf4ae0e45a02b4ca5a2f4cd002a1263bc
didwogh132 <62732120+didwogh132@users.noreply.github.com>
2026-02-01 20:19:25 +0900

    [Gold III] Title: 슬슬 가지를 먹지 않으면 죽는다, Time: 160 ms, Memory: 12152 KB -BaekjoonHub

---
+++ 백준/Gold/27945. 슬슬 가지를 먹지 않으면 죽는다/슬슬 가지를 먹지 않으면 죽는다.cc
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct node {
+    int u, v, day;
+    bool operator<(const node& right) {
+        return day < right.day;
+    }
+};
+namespace algo {
+    namespace ds {
+        class UNION {
+        private:
+            vector<int> parent;
+            vector<int> sz;
+
+        public:
+            UNION(int n) {
+                parent.resize(n + 1);
+                sz.resize(n + 1, 1);
+
+                for (int i = 0; i <= n; i++) parent[i] = i;
+            }
+
+            int find(int x) {
+                if (parent[x] == x) return x;
+                parent[x] = find(parent[x]);
+                return parent[x];
+            }
+
+            bool unite(int a, int b) {
+                int ra = find(a);
+                int rb = find(b);
+
+                if (ra == rb) return false;
+
+                if (sz[ra] < sz[rb]) {
+                    int temp = ra;
+                    ra = rb;
+                    rb = temp;
+                }
+                parent[rb] = ra;
+                sz[ra] += sz[rb];
+                return true;
+            }
+        };
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    algo::ds::UNION dsu(n);
+
+    vector<node> arr;
+    int ans = 1;
+    for (int i = 0; i < m; i++) {
+        int u, v, day;
+        cin >> u >> v >> day;
+        arr.push_back({ u, v, day });
+    }
+    sort(arr.begin(), arr.end());
+
+    for (int i = 0; i < m; i++) {
+        if (arr[i].day > ans) break;
+        if (arr[i].day == ans) {
+            if (!dsu.unite(arr[i].u, arr[i].v)) {
+                break;
+            }
+            ans++;
+            if (ans > n) break;
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
